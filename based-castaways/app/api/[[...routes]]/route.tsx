487f6868d2bbd897504612ff48ce3418e6a64837
Asylumreid <2201103@sit.singaporetech.edu.sg>
2024-04-12 15:12:35 +0800

    final changes please
    !

--- based-castaways/app/api/[[...routes]]/route.tsx
+++ based-castaways/app/api/[[...routes]]/route.tsx
@@ -52,45 +52,6 @@ app.frame('/', (c) => {
   });
 });
 
-// app.frame('/first', (c) => {
-//   const { buttonValue } = c;
-//   const state = c.deriveState(previousState => {
-//     if (buttonValue === '1') previousState.points += 1;
-//     if (buttonValue === '2') previousState.points += 2;
-//     if (buttonValue === '3') previousState.points += 3;
-//     if (buttonValue === '4') previousState.points += 4;
-//   });
-//   return c.res({
-//     action:'/second',
-//     image: (
-//       <div
-//       style={{
-//         display: 'flex', // Ensuring proper handling of child components
-//         flexDirection: 'column', // Organizes children vertically
-//         color: 'white',
-//         fontSize: 60,
-//         fontStyle: 'normal',
-//         letterSpacing: '-0.025em',
-//         lineHeight: 1.4,
-//         marginTop: 30,
-//         padding: '0 120px',
-//         whiteSpace: 'pre-wrap',
-//       }}
-//     >
-//       hello     
-//       </div>
-//     image: (
-//       <div>hello</div>
-//     ),
-//     intents: [
-//       <Button value="1">1</Button>,
-//       <Button value="2">2</Button>,
-//       <Button value="3">3</Button>,
-//       <Button value="4">4</Button>,
-//     ],
-//   });
-// });
-
 app.frame('/first', (c) => {
   const { buttonValue } = c
   const state = c.deriveState(previousState => {
@@ -306,31 +267,44 @@ function determinePersonality(points) {
 
 app.frame('/personality1', (c) => {
   return c.res({
-    image: 'https://coral-grieving-porpoise-728.mypinata.cloud/ipfs/QmeFj4xhH2Rb39jyFxRd9qgbC35bepjMYsJyPeSZYnrdUP'
+    image: ('https://coral-grieving-porpoise-728.mypinata.cloud/ipfs/QmeFj4xhH2Rb39jyFxRd9qgbC35bepjMYsJyPeSZYnrdUP'),
+    intents: [
+      <Button.Link href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>Mint</Button.Link>,
+      <Button.Reset>Retry</Button.Reset>,    
+    ],
   });
 });
 
 
 app.frame('/personality2', (c) => {
   return c.res({
-    image: 'https://coral-grieving-porpoise-728.mypinata.cloud/ipfs/QmYE8oH8DssHXoeTnhV1L2jfuAgSW33fLXG293Ws9NqKGg'
+    image: ('https://coral-grieving-porpoise-728.mypinata.cloud/ipfs/QmYE8oH8DssHXoeTnhV1L2jfuAgSW33fLXG293Ws9NqKGg'),
+    intents: [
+      <Button.Link href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>Mint</Button.Link>,
+      <Button.Reset>Retry</Button.Reset>,
+    ],
   });
 });
 
 app.frame('/personality3', (c) => {
   return c.res({
-    image: 'https://coral-grieving-porpoise-728.mypinata.cloud/ipfs/QmcTv7LwT2AvWqT1vXrUp8QrLNUUyH6ssidUwNNYqJZono'
+    image: ('https://coral-grieving-porpoise-728.mypinata.cloud/ipfs/QmcTv7LwT2AvWqT1vXrUp8QrLNUUyH6ssidUwNNYqJZono'),
+    intents: [
+      <Button.Link href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>Mint</Button.Link>,
+      <Button.Reset>Retry</Button.Reset>,
+    ],
   });
 });
 
 app.frame('/personality4', (c) => {
   return c.res({
-    image: 'https://coral-grieving-porpoise-728.mypinata.cloud/ipfs/QmVxruik75m7uxRsKpZJcH1L2htxAYw67RM1Afy8GcqhPL'
+    image: ('https://coral-grieving-porpoise-728.mypinata.cloud/ipfs/QmVxruik75m7uxRsKpZJcH1L2htxAYw67RM1Afy8GcqhPL'),
+    intents: [
+      <Button.Link href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>Mint</Button.Link>,
+      <Button.Reset>Retry</Button.Reset>,
+    ],
   });
 });
-
-
-
 
 devtools(app, { serveStatic })
 
